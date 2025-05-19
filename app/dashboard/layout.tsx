38137975f5bb39e2ce24837cde786a4b59e4385b
wahid-mahroof <wahidmahroof040@gmail.com>
2025-05-19 09:53:59 -0700

    sidebar dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import LeftSidebar from "@/components/dashboard/left-sidebar";
+import React from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen w-full">
+      <div className="flex">
+        <LeftSidebar />
+        <div className="flex">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default layout;
