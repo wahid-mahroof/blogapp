bf8a262cbec088bcb6aca869ac440d5618758017
wahid-mahroof <wahidmahroof040@gmail.com>
2025-05-24 10:37:40 -0700

    work on dashboard

--- components/dashboard/blog-dashboard.tsx
+++ components/dashboard/blog-dashboard.tsx
@@ -4,7 +4,12 @@ const BlogDashboard = () => {
   return (
     <div>
       <main className="flex-1 p-4 md:p-8">
-        <div className="flex justify-between items-center mb-8"></div>
+        <div className="flex justify-between items-center mb-8">
+          <div>
+            <h1 className="font-bold text-2xl">blog Dashboard</h1>
+            <p>Manage your content and analytic</p>
+          </div>
+        </div>
       </main>
     </div>
   );
