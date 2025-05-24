e51ef22bad76bcdaeb40496c9947bea925b93827
wahid-mahroof <wahidmahroof040@gmail.com>
2025-05-24 10:36:22 -0700

    work on dashboard

--- components/dashboard/blog-dashboard.tsx
+++ components/dashboard/blog-dashboard.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const BlogDashboard = () => {
-  return <div>BlogDashboard</div>;
+  return (
+    <div>
+      <main className="flex-1 p-4 md:p-8">
+        <div className="flex justify-between items-center mb-8"></div>
+      </main>
+    </div>
+  );
 };
 
 export default BlogDashboard;
