789554870bb7e9cf33afa2c3c195826c61a85672
wahid-mahroof <wahidmahroof040@gmail.com>
2025-05-29 05:08:09 -0700

    validation

--- actions/create-article.ts
+++ actions/create-article.ts
@@ -1,4 +1,5 @@
 "use server ";
+import { Cagliostro } from "next/font/google";
 import { z } from "Zod";
 
 const createArticleSchema = z.object({
@@ -6,10 +7,23 @@ const createArticleSchema = z.object({
   category: z.string().min(3).max(50),
   content: z.string().min(10),
 });
+
+type createArticlesFormstatec = {
+  error: {
+    title?: string[];
+    Category?: string[];
+    featuredImage?: string[];
+    content?: string[];
+    formError?: string[];
+  };
+};
 export const createArticle = async (formData: FormData) => {
   const result = createArticleSchema.safeParse({
     title: formData.get("title"),
     category: formData.get("category"),
     content: formData.get("content"),
   });
+
+  if (!result.success) {
+  }
 };
