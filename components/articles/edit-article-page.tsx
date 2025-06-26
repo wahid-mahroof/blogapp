2027f52d3429810eabb5aa9ef103fd3f93930b24
wahid-mahroof <wahidmahroof040@gmail.com>
2025-06-26 08:29:20 -0700

    create eidt article

--- components/articles/edit-article-page.tsx
+++ components/articles/edit-article-page.tsx
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { FormEvent, startTransition, useActionState, useState } from "react";
+import { CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Input } from "../ui/input";
+import { Label } from "@radix-ui/react-dropdown-menu";
+import dynamic from "next/dynamic";
+import { Button } from "../ui/button";
+import "react-quill-new/dist/quill.snow.css";
+import { createArticle } from "@/actions/create-article";
+import { error } from "console";
 
-const EditArticlePage = () => {
-  return <div>EditArticlePage</div>;
-};
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
 
-export default EditArticlePage;
+function CreateArticlesPage() {
+  const [content, setContent] = useState();
+  const [formState, action, isPending] = useActionState(createArticle, {
+    error: {},
+  });
+
+  const handlesubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+  
+  FormData.append("content",content);
+
+  startTransition(() => {
+    action(FormData);
+  })
+}
+  return (
+    <div className="max-4 mx-auto p-6">
+      <CardHeader>
+        <CardTitle>Create New Article</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handlesubmit} className="space-y-6">
+          <div className="space-y-2">
+            <Input
+              type="text"
+              name="title"
+              placeholder="Enter a article title"
+            />
+            {formState.error.title && (
+              <span className="text-red-600 text-sm">
+                {formState.error.title}
+              </span>
+            )}
+          </div>
+          <Label>Category</Label>
+          <div>
+            <select className="flex h-10 w-full rounded-md" name="category" id="category">
+              <option value="">Select category</option>
+              <option value="technology">technology</option>
+              <option value="programming">Programming</option>
+              <option value="web-debelopment">Web dvelopment</option>
+            </select>
+            {formState.error.Category && (
+              <span className="text-red-600 text-sm">
+                {formState.error.Category}
+              </span>
+          </div>
+          <div className="space-y-2">
+            <Label>featured Image</Label>
+            <Input
+              type="file"
+              id="featuredImage"
+              name="featuredImage"
+              accept="image/*"
+            />
+          </div>
+          <div className="space-y-2">
+            <Label>Content</Label>
+            <ReactQuill theme="snow" value={content} onChange={setContent} />
+            {formState.error.content && (
+              <span className="text-red-600 text-sm">
+                {formState.error.content[0]}
+              </span>
+          </div>
+          <div className="flex justify-end gap-4">
+            <Button variant={"outline"}>Cancel</Button>
+            <Button type="submit" disabled={isPending}>
+              {
+                isPending ? "Loading..." : "Public Article"
+              }
+            </Button>
+          </div>
+        </form>
+      </CardContent>
+    </div>
+  );
+}
+
+export default CreateArticlesPage;
