f7c160a5eee5cb19cefe520fb6715d198b876c3e
wahid-mahroof <wahidmahroof040@gmail.com>
2025-07-01 00:42:40 -0700

    id update

--- components/articles/[id]/edit/page.tsx
+++ components/articles/[id]/edit/page.tsx
@@ -3,19 +3,19 @@ import EditArticlePage from "../../edit-article-page";
 import { Prisma } from "@/lib/prisma";
 
 type EditArticleParams = {
-  params:Promise<{id:string}>
-}
+  params: Promise<{ id: string }>;
+};
 
-const  page :React.FC<EditArticleParams>  async ({params}) => {
+const page: React.FC<EditArticleParams> = async ({ params }) => {
   const id = (await params).id;
   const article = await Prisma.article.findunique({
-    where:{id}
-  })
+    where: { id },
+  });
   return (
     <div>
       <EditArticlePage />
     </div>
   );
-}
+};
 
 export default page;
