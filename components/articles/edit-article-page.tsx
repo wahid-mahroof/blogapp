afc709227d828272123494a0e2c2dc10cd09317c
wahid-mahroof <wahidmahroof040@gmail.com>
2025-07-05 04:06:53 -0700

    id category

--- components/articles/edit-article-page.tsx
+++ components/articles/edit-article-page.tsx
@@ -77,6 +77,9 @@ const EditArticlePage : React.FC<EditArticleProps> = ({article}) =>  {
               accept="image/*"
               
             />
+            <div>
+              
+            </div>
             <Image 
             src={article.featuredImage}
             alt="featured-image"
