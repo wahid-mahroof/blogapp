430a8a031eba5fcf67356e5e9996f5327ed91da4
wahid-mahroof <wahidmahroof040@gmail.com>
2025-07-05 04:05:42 -0700

    id category

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "img.freepik.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.clodinary.com",
+      },
     ],
   },
 };
