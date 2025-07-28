6d78c80fa3277ed89cbc069e8da1560c20879b23
wahid-mahroof <wahidmahroof040@gmail.com>
2025-07-28 13:36:07 -0700

    recent article edit

--- actions/delete-article.ts
+++ actions/delete-article.ts
@@ -1,9 +1,11 @@
 "use server";
 
 import { Prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 export const deleteArticle = async (articleId: string) => {
   await Prisma.article.delete({
     where: { id: articleId },
   });
+  revalidatePath("/dashboard");
 };
