80d8c99fc57a162551a25c0828f9acc2c4609cf0
wahid-mahroof <wahidmahroof040@gmail.com>
2025-07-30 14:32:45 -0700

    loading page

--- dashboard/loading.tsx
+++ dashboard/loading.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const DashboardLoadingScreen = () => {
-  return <div className="flex items-center justify-center min-h-screen"></div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-50 to-purple-500"></div>
+  );
 };
 
 export default DashboardLoadingScreen;
