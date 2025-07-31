29bc6ae8a0093aa426ba07083d94eb0495dae37b
wahid-mahroof <wahidmahroof040@gmail.com>
2025-07-31 14:36:12 -0700

    loading page

--- dashboard/loading.tsx
+++ dashboard/loading.tsx
@@ -5,6 +5,7 @@ const DashboardLoadingScreen = () => {
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-50 to-purple-500">
       <div className="text-center flex flex-col items-center">
         {/* spinner*/}
+        <div className="animate-spin rounded-full h-16 w-16 border-t--4 border-b-4 border-purple-500 dark:border-2"></div>
       </div>
     </div>
   );
