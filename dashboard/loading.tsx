5248fa9f185c3cbcc0df2f576026e16b4444f273
wahid-mahroof <wahidmahroof040@gmail.com>
2025-07-31 14:34:21 -0700

    loading page

--- dashboard/loading.tsx
+++ dashboard/loading.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const DashboardLoadingScreen = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-50 to-purple-500">
-      <div className="text-center flex flex-col">{/* spinner*/}</div>
+      <div className="text-center flex flex-col items-center">
+        {/* spinner*/}
+      </div>
     </div>
   );
 };
