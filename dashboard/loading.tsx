cace1b27a8d2c4bd572cf5426e9769e1312bc61e
wahid-mahroof <wahidmahroof040@gmail.com>
2025-08-01 08:01:49 -0700

    loading page

--- dashboard/loading.tsx
+++ dashboard/loading.tsx
@@ -10,7 +10,9 @@ const DashboardLoadingScreen = () => {
           <h1 className="text-2xl font-se,ibold text-gray-800 dark:text-gray-200">
             loading dashboard...
           </h1>
-          <p className="">please wait while we prepare your dashboard.</p>
+          <p className="text-gray-600 dark:text-gray-400 mt-2">
+            please wait while we prepare your dashboard.
+          </p>
         </div>
       </div>
     </div>
