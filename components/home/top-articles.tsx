0ae2c0964ac0f31dace8000457aebb5a4e6b548a
wahid-mahroof <wahidmahroof040@gmail.com>
2025-08-02 14:02:08 -0700

    loading page

--- components/home/top-articles.tsx
+++ components/home/top-articles.tsx
@@ -45,7 +45,7 @@ const TopArticles = async () => {
 
             <div className="flex items-center gap-x-3 mt-4 text-sm text-muted-foreground">
               <Avatar>
-                <AvatarImage src={} />
+                <AvatarImage src={article.author.imageUrl} />
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
               <span> wahid Mahroof</span>
