2b4f713bba506136aa7ff86669f28425f21c34bc
wahid-mahroof <wahidmahroof040@gmail.com>
2025-08-02 14:00:21 -0700

    loading page

--- components/home/top-articles.tsx
+++ components/home/top-articles.tsx
@@ -8,7 +8,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { Prisma } from "@/lib/prisma";
 
 const TopArticles = async () => {
-  const article = await Prisma.articles.findMany({
+  const articles = await Prisma.articles.findMany({
     orderBy: {
       createdAt: "desc",
     },
@@ -25,42 +25,47 @@ const TopArticles = async () => {
   });
   return (
     <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-      <Card className="group relative overflow-hidden transition-all hover:scale-[1.02] border border-gray-200 dark:border-white/10 bg-white dark:bg-gray-900 backdrop-blur-lg">
-        <div className="p-6">
-          <Link href="/articles/1">
-            <div className="relative h-48 w-full overflow-hidden rounded-xl">
-              <Image
-                src="https://img.freepik.com/free-photo/side-view-female-asian-woman-sitting-cafe-working-laptop_1098-17167.jpg"
-                alt="article"
-                fill
-                className="object-cover"
-                priority
-              />
+      {articles.slice(
+        0,
+        3
+      )((article) => (
+        <Card className="group relative overflow-hidden transition-all hover:scale-[1.02] border border-gray-200 dark:border-white/10 bg-white dark:bg-gray-900 backdrop-blur-lg">
+          <div className="p-6">
+            <Link href="/articles/1">
+              <div className="relative h-48 w-full overflow-hidden rounded-xl">
+                <Image
+                  src="https://img.freepik.com/free-photo/side-view-female-asian-woman-sitting-cafe-working-laptop_1098-17167.jpg"
+                  alt="article"
+                  fill
+                  className="object-cover"
+                  priority
+                />
+              </div>
+            </Link>
+
+            <div className="flex items-center gap-x-3 mt-4 text-sm text-muted-foreground">
+              <Avatar>
+                <AvatarImage src="/avatar1.jpg" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <span> wahid Mahroof</span>
             </div>
-          </Link>
 
-          <div className="flex items-center gap-x-3 mt-4 text-sm text-muted-foreground">
-            <Avatar>
-              <AvatarImage src="/avatar1.jpg" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <span> wahid Mahroof</span>
-          </div>
+            <h3 className="mt-4 text-xl font-semibold text-gray-900 dark:text-white">
+              How to become frontend developer in 2024
+            </h3>
 
-          <h3 className="mt-4 text-xl font-semibold text-gray-900 dark:text-white">
-            How to become frontend developer in 2024
-          </h3>
-
-          <p className="mt-2 text-gray-600 dark:text-white">Web devlopment</p>
-          <div
-            className="mt-6 flex items-center
+            <p className="mt-2 text-gray-600 dark:text-white">Web devlopment</p>
+            <div
+              className="mt-6 flex items-center
            justify-between text-sm text-gray-500 dark:text-gray-400"
-          >
-            <span className="">12 feb</span>
-            <span className="">{12} min to read</span>
+            >
+              <span className="">12 feb</span>
+              <span className="">{12} min to read</span>
+            </div>
           </div>
-        </div>
-      </Card>
+        </Card>
+      ))}
     </div>
   );
 };
